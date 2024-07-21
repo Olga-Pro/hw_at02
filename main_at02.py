7bb2163c39574afb04b6d195b1ed04327f07f6b0
Olga-Pro <oip_@inbox.ru>
2024-07-21 13:51:08 +0300

    add code

--- main_at02.py
+++ main_at02.py
@@ -1,5 +1,8 @@
-import pytest
-
-
-def check(number):
-    return number % 2 == 0
+# Подсчет количества гласных в строке
+def count_vowels(s: str) -> int:
+    vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯaeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
--- test_at02.py
+++ test_at02.py
@@ -1,5 +1,17 @@
 import pytest
-from main_at02 import check
+from main_at02 import count_vowels
 
-if __name__ == "__main__":
-    unittest.main()
+def test_only_vowels():  # Только гласные
+    assert count_vowels("аеёиоуыэюяAEIOU") == 15
+
+def test_no_vowels():  # Только согласные
+    assert count_vowels("bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ") == 0
+
+def test_mixed_string():  # Гласные и согласные
+    assert count_vowels("Привет, как дела? Hello, how are you?") == 12
+
+def test_empty_string():  # Пустая строка
+    assert count_vowels("") == 0
+
+def test_upper_and_lower_case():  # Гласные и согласные - прописные и строчные
+    assert count_vowels("АаБвГдЕеЖзИиОоУуЫыЭэЮюЯя") == 18
